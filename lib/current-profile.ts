02d8a3e031159fe8e94570f3d2d5f54cd93f7bd1
ShivamSpm <spmshivam@gmail.com>
2024-05-03 21:38:15 -0400

    Setup of socket

--- lib/current-profile.ts
+++ lib/current-profile.ts
@@ -2,13 +2,11 @@ import { auth } from "@clerk/nextjs/server";
 import { db } from "./db";
 
 export const currentProfile = async () => {
-  console.log("userId");
   const { userId } = auth();
   if (!userId) {
     return null;
   }
-  console.log("userId");
-  console.log(userId);
+
   const profile = await db.profile.findUnique({
     where: {
       userId: userId,
--- types.ts
+++ types.ts
@@ -1,9 +1,18 @@
 import { Member, Profile, Server } from "@prisma/client";
 import { Server as NetServer, Socket } from "net";
 import { NextApiResponse } from "next";
+import { Server as SocketIOServer } from "socket.io";
 
 export type ServerWithMemberAndProfile = Server & {
   members: (Member & { profile: Profile })[];
 };
 
 export type MemberWithProfile = (Member & { profile: Profile })
+
+export type NextApiResponseServerIO = NextApiResponse & {
+  socket: Socket & {
+    server: NetServer & {
+      io: SocketIOServer;
+    };
+  };
+};
